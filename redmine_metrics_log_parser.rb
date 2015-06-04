5f2b1e036390b59564810df93049b27cee7f1bd4
AlexeyAlexey <alexey.kondratenko@mail.ru>
2015-06-04 20:32:29 +0300

    add fields

--- redmine_metrics_log_parser.rb
+++ redmine_metrics_log_parser.rb
@@ -19,21 +19,27 @@ while x = gets
     sql = ""
     name = /=>name=(.*?)<=/.match(x)
     if name[1] == "process_action.action_controller"
-      values = /=>name=(.*?)<==>transaction_id=(.*?)<==>start_time=(.*?)<==>end_time=(.*?)<==>duration=(.*?)<==>view_runtime=(.*?)<==>db_runtime=(.*?)<==>payload=(.*?)<=/.match(x)
+      values = /=>name=(.*?)<==>transaction_id=(.*?)<==>controller=(.*?)<==>action=(.*?)<==>status=(.*?)<==>start_time=(.*?)<==>end_time=(.*?)<==>duration=(.*?)<==>view_runtime=(.*?)<==>db_runtime=(.*?)<==>payload=(.*?)<=/.match(x)
       sql = "INSERT INTO action_controller_loggers (transaction_id, 
+                                                    controller,
+                                                    action,
+                                                    status,
                                                     start_time, 
                                                     end_time, 
                                                     duration, 
                                                     view_runtime,
                                                     db_runtime,
                                                     payload) 
-                                                  VALUES('#{values[2]}', 
-                                                         '#{values[3]}', 
+                                                  VALUES('#{values[2]}',
+                                                         '#{values[3]}',
                                                          '#{values[4]}', 
                                                          '#{values[5]}', 
-                                                         '#{values[6]}', 
+                                                         '#{values[6]}',
                                                          '#{values[7]}', 
-                                                         '#{values[8]}' );"
+                                                         '#{values[8]}', 
+                                                         '#{values[9]}', 
+                                                         '#{values[10]}', 
+                                                         '#{values[11]}' );"
     else
       values = /=>name=(.*?)<==>transaction_id=(.*?)<==>start_time=(.*?)<==>end_time=(.*?)<==>duration=(.*?)<==>payload=(.*?)<=/.match(x)
       sql = "INSERT INTO action_view_loggers (transaction_id, 
